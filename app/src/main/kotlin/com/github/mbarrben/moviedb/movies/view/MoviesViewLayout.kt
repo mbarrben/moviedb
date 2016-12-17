7ecd733f383a8f2dfc39b358fcafe90071145200
Miguel Barrios <mbarrben@gmail.com>
2016-12-17 21:00:28 +0100

    Extract pagination scroll utils to extensions

--- app/src/main/kotlin/com/github/mbarrben/moviedb/movies/view/MoviesViewLayout.kt
+++ app/src/main/kotlin/com/github/mbarrben/moviedb/movies/view/MoviesViewLayout.kt
@@ -3,22 +3,18 @@ package com.github.mbarrben.moviedb.movies.view
 import android.content.Context
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.GridLayoutManager.SpanSizeLookup
-import android.support.v7.widget.LinearLayoutManager
-import android.support.v7.widget.RecyclerView
-import android.support.v7.widget.RecyclerView.LayoutManager
 import android.util.AttributeSet
 import android.widget.RelativeLayout
 import com.github.mbarrben.moviedb.R
 import com.github.mbarrben.moviedb.domain.movies.MoviesView
 import com.github.mbarrben.moviedb.extensions.Timber
 import com.github.mbarrben.moviedb.extensions.inflate
+import com.github.mbarrben.moviedb.extensions.scrollToEndEvents
 import com.github.mbarrben.moviedb.model.entities.Movie
 import com.github.mbarrben.moviedb.movies.view.adapter.MoviesAdapter
 import com.github.mbarrben.moviedb.movies.view.adapter.Type
 import com.github.mbarrben.moviedb.movies.view.adapter.Type.LOADING
 import com.github.mbarrben.moviedb.movies.view.adapter.Type.MOVIE
-import com.jakewharton.rxbinding.support.v7.widget.RecyclerViewScrollEvent
-import com.jakewharton.rxbinding.support.v7.widget.scrollEvents
 import kotlinx.android.synthetic.main.movies_view.view.moviesRecycler
 import rx.Observable
 
@@ -64,23 +60,10 @@ class MoviesViewLayout(context: Context, attrs: AttributeSet) : RelativeLayout(c
 
   override fun showError() = hideLoading()
 
-  override fun paginationEvents(): Observable<Int> = moviesRecycler.scrollEvents()
+  override fun paginationEvents(): Observable<Int> = moviesRecycler.scrollToEndEvents(VISIBLE_THRESHOLD)
       .filter { !moviesAdapter.loading }
-      .filter { it.isScrollingUp() }
-      .filter { moviesRecycler.lastXItemsAreVisible(VISIBLE_THRESHOLD) }
       .map { moviesAdapter.movies.page + 1 }
       .doOnEach { showLoading() }
       .doOnEach { Timber.d { "infinite scroll" } }
-
-  private fun RecyclerViewScrollEvent.isScrollingUp() = dy() > 0
-
-  private fun RecyclerView.lastXItemsAreVisible(x: Int) = with(layoutManager) {
-    itemCount <= lastVisibleItemPosition() + x
-  }
-
-  private fun LayoutManager.lastVisibleItemPosition() = when (this) {
-    is LinearLayoutManager -> findLastCompletelyVisibleItemPosition()
-    else                   -> 0
-  }
 
 }
