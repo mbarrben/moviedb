e8197554614dde27cc618a3826ace121ec31d39f
mbarrben <mbarrben@gmail.com>
2016-12-28 18:47:59 +0100

    Add all movie fields to model

--- model/src/main/kotlin/com.github.mbarrben.moviedb.model/rest/RestMovieRepository.kt
+++ model/src/main/kotlin/com.github.mbarrben.moviedb.model/rest/RestMovieRepository.kt
@@ -2,6 +2,7 @@ package com.github.mbarrben.moviedb.model.rest
 
 import com.github.mbarrben.moviedb.model.MovieRepository
 import com.github.mbarrben.moviedb.model.entities.Movie
+import com.google.gson.GsonBuilder
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
@@ -10,13 +11,21 @@ import rx.Observable
 
 class RestMovieRepository(client: OkHttpClient, val apiKey: String) : MovieRepository {
 
-  private val api = Retrofit.Builder()
-      .baseUrl(MovieDatabaseAPI.BASE_URL)
-      .client(client)
-      .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-      .addConverterFactory(GsonConverterFactory.create())
-      .build()
-      .create(MovieDatabaseAPI::class.java)
+  private val api: MovieDatabaseAPI
+
+  init {
+    val gson = GsonBuilder().setDateFormat("yyyy-MM-dd")
+        .create()
+
+    api = Retrofit.Builder()
+        .baseUrl(MovieDatabaseAPI.BASE_URL)
+        .client(client)
+        .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+        .addConverterFactory(GsonConverterFactory.create(gson))
+        .build()
+        .create(MovieDatabaseAPI::class.java)
+  }
+
 
   override fun getMovies(page: Int): Observable<Movie.List> = api.popular(apiKey, page)
       .map { Movie.List(it.page, it.results) }
