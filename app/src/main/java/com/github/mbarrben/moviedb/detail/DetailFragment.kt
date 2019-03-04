98cf8c14d754f64d0fbdb47b7a3c5ad32de42434
Miguel Barrios <mbarrben@gmail.com>
2019-03-04 14:37:10 +0100

    Render movie data in detail screen

--- app/src/main/java/com/github/mbarrben/moviedb/detail/DetailFragment.kt
+++ app/src/main/java/com/github/mbarrben/moviedb/detail/DetailFragment.kt
@@ -1,20 +1,16 @@
 package com.github.mbarrben.moviedb.detail
 
-import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.github.mbarrben.moviedb.BaseFragment
 import com.github.mbarrben.moviedb.R
-import timber.log.Timber
+import com.github.mbarrben.moviedb.databinding.DetailFragmentBinding
 
-class DetailFragment : Fragment() {
+class DetailFragment : BaseFragment<DetailFragmentBinding>(R.layout.detail_fragment) {
 
     private val args: DetailFragmentArgs by navArgs()
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        Timber.tag("detail").d("movie detail: $args.movie")
-        return inflater.inflate(R.layout.detail_fragment, container, false)
+    override fun onBind(binding: DetailFragmentBinding) {
+        binding.movie = args.movie
     }
+
 }
