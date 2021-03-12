ef792d3035f8a4d4c6e3ca0c0e5313aec6cb1ca6
Miguel Barrios <miguel.barriosbenito@telefonica.com>
2021-03-12 23:04:05 +0100

    Check Glide

--- app/src/main/java/com/github/mbarrben/moviedb/commons/extensions/BindingAdapters.kt
+++ app/src/main/java/com/github/mbarrben/moviedb/commons/extensions/BindingAdapters.kt
@@ -5,7 +5,9 @@ import android.view.View.GONE
 import android.view.View.VISIBLE
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
+import com.bumptech.glide.Glide
 import com.squareup.picasso.Picasso
+import timber.log.Timber
 
 @BindingAdapter("goneUnless")
 fun View.setGoneUnless(isVisible: Boolean) {
@@ -14,7 +16,31 @@ fun View.setGoneUnless(isVisible: Boolean) {
 
 @BindingAdapter("url")
 fun ImageView.setUrl(url: String?) {
-    Picasso.get()
+    var time = System.currentTimeMillis()
+
+//    Picasso.get()
+//        .also {
+//            Timber.d("imageloader get ${System.currentTimeMillis() - time} ms")
+//            time = System.currentTimeMillis()
+//        }
+//        .load(url)
+//        .also {
+//            Timber.d("imageloader load ${System.currentTimeMillis() - time} ms")
+//            time = System.currentTimeMillis()
+//        }
+//        .into(this)
+
+    Glide.with(this)
+        .also {
+            Timber.d("imageloader with ${System.currentTimeMillis() - time} ms")
+            time = System.currentTimeMillis()
+        }
         .load(url)
+        .also {
+            Timber.d("imageloader load ${System.currentTimeMillis() - time} ms")
+            time = System.currentTimeMillis()
+        }
         .into(this)
+
+    Timber.d("imageloader into ${System.currentTimeMillis() - time} ms")
 }
