6ecf0c09f2a405f932016714cab8edc48cc73789
Miguel Barrios <mbarrben@gmail.com>
2021-03-13 13:44:42 +0100

    Move movies repo and service to viewmodel component

--- app/src/main/java/com/github/mbarrben/moviedb/di/NetworkModule.kt
+++ app/src/main/java/com/github/mbarrben/moviedb/di/NetworkModule.kt
@@ -1,9 +1,65 @@
 package com.github.mbarrben.moviedb.di
 
+import android.content.Context
 import com.github.mbarrben.moviedb.BuildConfig
+import com.github.mbarrben.moviedb.commons.network.DateAdapter
+import com.github.mbarrben.moviedb.commons.network.DefaultHeadersInterceptor
+import com.github.mbarrben.moviedb.commons.network.ImageUrlAdapter
+import com.jakewharton.byteunits.DecimalByteUnit
+import com.squareup.moshi.Moshi
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import okhttp3.Cache
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import timber.log.Timber
+import java.io.File
 
+@Module
+@InstallIn(SingletonComponent::class)
 object NetworkModule {
     const val API_KEY = BuildConfig.API_KEY
-    const val BASE_URL = "https://api.themoviedb.org/3/"
-    const val POSTER_PREFIX = "https://image.tmdb.org/t/p/w500"
+
+    private const val BASE_URL = "https://api.themoviedb.org/3/"
+    private const val POSTER_PREFIX = "https://image.tmdb.org/t/p/w500"
+
+    @Provides
+    fun provideRetrofit(@ApplicationContext context: Context): Retrofit {
+        val dateAdapter = DateAdapter("yyyy-MM-dd")
+        val imageUrlAdapter = ImageUrlAdapter(POSTER_PREFIX)
+
+        val moshi = Moshi.Builder()
+            .add(dateAdapter)
+            .add(imageUrlAdapter)
+            .build()
+
+        val cache = Cache(
+            File(context.cacheDir, "http"),
+            DecimalByteUnit.MEGABYTES.toBytes(50)
+        )
+
+        val logger = object : HttpLoggingInterceptor.Logger {
+            override fun log(message: String) {
+                Timber.tag("Retrofit").d(message)
+            }
+        }
+        val loggingInterceptor = HttpLoggingInterceptor(logger).setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val okhttpClient = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .addInterceptor(DefaultHeadersInterceptor())
+            .cache(cache)
+            .build()
+
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(MoshiConverterFactory.create(moshi))
+            .client(okhttpClient)
+            .build()
+    }
 }
